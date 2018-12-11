d56f256da7f68673cdbf2e07122251e8f851a5de
Ajit Nath <ajit.nath@organicgrowthfunds.com>
2018-12-11 18:29:50 +0800

    Clean up csv reader

--- dropboxutils/readers.py
+++ dropboxutils/readers.py
@@ -30,31 +30,43 @@ CSVReadConfig = namedtuple(
 )
 
 
-def read_csv(filepath: str, csv_read_config: CSVReadConfig):
+def read_csv(filepath: str, csv_read_config: CSVReadConfig) -> pd.DataFrame:
+    '''
+    Read CSV from a url
+    ARGS:
+        filepath: Path to the file to be read
+        csv_read_config: File configurations for reading the csv file
+    '''
     dropbox_link = make_file_link(filepath)
     bytes_io = download_bytes_io(dropbox_link)
     df = read_csv_from_bytes_io(bytes_io, csv_read_config)
     return df
 
 
-def read_csv_from_bytes_io(bytes_io: io.BytesIO, csv_read_config: csv_read_config):
+def read_csv_from_bytes_io(bytes_io: io.BytesIO, csv_read_config: csv_read_config) -> pd.DataFrame:
+    '''
+    Read CSV from a bytes io instance
+    ARGS:
+        bytes_io: A bytes IO instance
+        csv_read_config: File configurations for reading the csv file
+    RETURNS:
+        df: Resulting pandas dataframe
+    '''
     df = pd.read_csv(bytes_io, sep=csv_read_config.seperator)
     df.columns = df.columns.astype('str')
     df.columns = df.columns.str.upper()
+    expected_cols = csv_read_config.expected_columns
+    df_cols = csv_read_config.df_columns
 
-    try:
-        validate_columns(df, csv_read_config)
-    except (ValueError, AssertionError, AttributeError) as err:
-        raise exceptions.ReaderException(err)
-
-    if csv_read_config.output_dataframe_columns:
-        df = df.rename(columns=dict(
-            zip(csv_read_config.expected_columns, csv_read_config.df_columns)
-        ))
+    if expected_cols is not None:
+        validate_columns(df, expected_cols)
+        df = df[expected_cols]
+    if df_cols is not None:
+        df = df.rename(columns=dict(zip(df.columns, df_cols)))
     if csv_read_config.index_col is not None:
-        dataframe = df.set_index(csv_read_config.index_col)
+        df = df.set_index(csv_read_config.index_col)
 
-    return dataframe
+    return df
 
 
 # Excel
@@ -81,20 +93,18 @@ def read_excel_from_bytes_io(bytes_io: io.BytesIO, excel_read_config: ExcelReadC
 
 # Validation
 
-def validate_columns(df, read_config):
-    if read_config.expected_columnns:
-        if not all(c in df.columns for c in read_config.expected_columns):
-            distinct_columns = set(read_config.expected_columns) - df.columns
-            raise AssertionError('Invalid columns - {}'.format(distinct_columns))
-        df = df[read_config.expected_columns]
-    else:
-        LOGGER.warning('No input columns specified, skipping validation')
-
-
-def rename_dataframe_columns(dataframe, input_columns, output_columns):
-    '''Rename_dataframe columns based on the result expected in output'''
-    dataframe = dataframe.rename(columns=dict(zip(input_columns, output_columns)))
-    return dataframe
+def validate_columns(df, expected_columns):
+    '''
+    Validate dataframe columns otherwise raise an appropriate
+    exceptions
+    ARGS:
+        df: Dataframe to be validated
+        expected_columns: Expected columns in the csv file
+    '''
+    distinct_cols = set(expected_columns) - df.columns
+    if distinct_cols:
+        raise exceptions.ReaderException('Invalid cols - {}'.format(distinct_cols))
+    LOGGER.debug('Dataframe validated')
 
 
 # Common
@@ -107,9 +117,9 @@ def make_file_link(filepath: str, max_tries=5) -> str:
             link = core.make_file_link(filepath)
             return link
         except exceptions.DropboxException as err:
-            LOGGER.warning('Unable to create file link - %s, retrying', filepath)
+            LOGGER.warning(err)
             if tries_count >= max_tries:
-                raise exceptions.MakeUrlException('Error creating file link - %s', filepath)
+                raise
         else:
             break
 
@@ -118,7 +128,5 @@ def download_bytes_io(link, read_timeout=10):
     try:
         filedata = io.BytesIO(requests.get(link, timout=read_timeout).content)
     except requests.exceptions.RequestsException as err:
-        raise exceptions.DownloadException(
-            'Data could not be read from URL - {} - {}'.format(url, err)
-        )
+        raise exceptions.DownloadException(err)
     return filedata
