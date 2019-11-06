1d39652ad9d72ccd17541918478d71de8e2ebed7
Ajit Nath <ajit.nath@organicgrowthfunds.com>
2019-11-06 11:42:08 +0800

    Increments version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dropboxutils',
-    version='0.4',
+    version='0.5',
     description='dropbox utitities for reading, writing and monitoring files on dropbox',
     packages=find_packages(),
     tests='tests',
