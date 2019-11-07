c4343ecde248c3d76019b12e0c07119bc2ab9490
Ajit Nath <ajit.nath@organicgrowthfunds.com>
2019-11-07 18:47:56 +0800

    Updates setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='dropbox-ds-AJIT-NATH',
-    version='0.5.1',
+    version='0.5.2',
     author='Ajit Nath',
     description='Provides data science utilities for dropbox',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    tests='tests',
+    tests='dropbox_ds/tests',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.7'
 )
