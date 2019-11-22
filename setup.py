7c04646348b335c754348112da695eb2d73a203a
Ajit Nath <ajit.nath@organicgrowthfunds.com>
2019-11-22 18:33:33 +0800

    Defines installation requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='dropbox-ds-AJIT-NATH',
-    version='0.5.6',
+    version='0.5.7',
     author='Ajit Nath',
     description='Provides data science utilities for dropbox',
     long_description=long_description,
@@ -20,6 +20,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'dropbox>=9.4.0',
+        'pandas>=0.25.3',
+        'xlrd>=1.2.0'
     ],
     python_requires='>=3.7'
 )
